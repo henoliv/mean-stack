1bd3764a8fea348310fd15804f96549940b10dee
Henrique Rodrigues <henrique@hostnet.com.br>
2017-07-26 11:25:58 -0300

    Iniciando Repositório com um teste no servidor Node.js

---
+++ node-server.js
@@ -0,0 +1,6 @@
+const http = require('http')
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Sou um servidor criado pelo Node.js\n')
+}).listen(3000, '127.0.0.1')
