35820baa8e9cc8cec64fff75f4367bd9d2235fa6
Henrique Rodrigues <henrique@hostnet.com.br>
2017-07-27 15:23:43 -0300

    Adicionando Bootstrap e tabela de contatos.

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -4,5 +4,11 @@ angular.module('contatooh').controller('ContatosController',
     $scope.total = 0
 
     $scope.incrementa = () => $scope.total++
+
+    $scope.contatos = [
+      { _id: 1, nome: 'Contato Angular 1', email: 'cont1@empresa.com.br' },
+      { _id: 2, nome: 'Contato Angular 2', email: 'cont2@empresa.com.br' },
+      { _id: 3, nome: 'Contato Angular 3', email: 'cont3@empresa.com.br' }
+    ]
   }
 )
